412f3c4afad509a04062590246bb8a3097f3ddc0
Christopher Konicki <82127510+SirChurchill9999@users.noreply.github.com>
2023-07-14 12:51:45 -0500

    added error handling and logging

--- testingShopifyApp.js
+++ testingShopifyApp.js
@@ -1,39 +1,50 @@
-//var shopifyAPI = require('shopify-node-api');
 import "@shopify/shopify-api/adapters/node";
-//import { shopify } from "@shopify/shopify-api";
-import { shopifyApi, ApiVersion, Session } from '@shopify/shopify-api';
+import { shopifyApi, ApiVersion, LogSeverity } from '@shopify/shopify-api';
 import {restResources} from '@shopify/shopify-api/rest/admin/2023-04'; // 2022-07
+import { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_ACCESS_TOKEN } from './secrets.js';
+import { writeFileSync } from "fs";
 
-var Shopify = new shopifyApi({
-  apiSecretKey: "9a2ff22ebe3e683c84188812127a2c5d",            // Note: this is the API Secret Key, NOT the API access token
+const errorLogFile = './error.log';
+const appLogFile = './application.log';
+
+const myLoggingFunction = (severity, message) => {
+  writeFileSync(appLogFile, `${message}\n`, {flag: 'a'});
+  if (severity == LogSeverity.Error) {
+    writeFileSync(errorLogFile, `${message}\n`, {flag: 'a'});
+  }
+};
+
+const Shopify = new shopifyApi({
+  apiSecretKey: SHOPIFY_API_SECRET_KEY,
   apiVersion: ApiVersion.April23,
-  isCustomStoreApp: true,                        // this MUST be set to true (default is false)
-  adminApiAccessToken: "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", // Note: this is the API access token, NOT the API Secret Key
+  isCustomStoreApp: true,
+  adminApiAccessToken: SHOPIFY_API_ACCESS_TOKEN,
   isEmbeddedApp: false,
   hostName: "hand-me-diamonds-staging.myshopify.com",
-  // Mount REST resources.
+  logger: {
+    myLoggingFunction,
+  },
   restResources
-
-  /*
-  shop: 'hand-me-diamonds-staging.myshopify.com', // MYSHOP.myshopify.com
-  shopify_api_key: 'd39567787cc8ffa48d498b40b2393477', // Your API key
-  access_token: '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' // Your API password
-  */
-
 });
 
-// Shopify.get('/admin/products.json', function(err, data, headers){
-//   console.log(data); // Data contains product json information
-//   console.log(headers); // Headers returned from request
-// });
+async function getProductCustomerOrderCounts() {
+  try {
+    const session = Shopify.session.customAppSession("hand-me-diamonds-staging.myshopify.com");
 
-const session = Shopify.session.customAppSession("hand-me-diamonds-staging.myshopify.com");
+    const { count: productCount } = await Shopify.rest.Product.count({ session });
+    const { count: customerCount } = await Shopify.rest.Customer.count({ session });
+    const { count: orderCount } = await Shopify.rest.Order.count({ session });
 
-// Use REST resources to make calls.
-const { count: productCount } = await Shopify.rest.Product.count({ session });
-const { count: customerCount } = await Shopify.rest.Customer.count({ session });
-const { count: orderCount } = await Shopify.rest.Order.count({ session });
+    console.log(
+      `There are ${productCount} products, ${customerCount} customers, and ${orderCount} orders in the ${session.shop} store.`
+    );
+  } catch (error) {
+    if (error instanceof HttpError && error.response.status === 429) {
+      console.error('Rate limit error:', error);
+    } else {
+      console.error('Unknown error:', error);
+    }
+  }
+}
 
-console.log(
-  `There are ${productCount} products, ${customerCount} customers, and ${orderCount} orders in the ${session.shop} store.`
-);
+getProductCustomerOrderCounts();
