8474a448f0bafd1d47608b8f2b21c681453936a3
Christopher Konicki <82127510+SirChurchill9999@users.noreply.github.com>
2023-07-21 19:15:58 -0500

    deleted

--- secrets.js
+++
@@ -1,7 +0,0 @@
-export const SHOPIFY_API_SECRET_KEY = "9a2ff22ebe3e683c84188812127a2c5d";
-export const SHOPIFY_API_ACCESS_TOKEN = "";
-export const STOREFRONT_API_ACCESS_TOKEN = "";
-export const NIVODA_STAGING_USERNAME = "testaccount@sample.com";
-export const NIVODA_STAGING_PASSWORD = "staging-nivoda-22";
-export const SHOP = "hand-me-diamonds-staging.myshopify.com";
-export const API_KEY = "d39567787cc8ffa48d498b40b2393477";
