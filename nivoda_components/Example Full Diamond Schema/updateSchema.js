d95dba2969242bd3e1bfbd02f1ee488bde6b044e
Christopher Konicki <ckonicki@uark.edu>
2024-03-17 11:28:04 -0500

    Refactor code for improved performance and readability

--- nivoda_components/Example Full Diamond Schema/updateSchema.js
+++ nivoda_components/Example Full Diamond Schema/updateSchema.js
@@ -1,13 +1,12 @@
 // Importing the node-fetch library to make HTTP requests
 import fetch from 'node-fetch'; // using node-fetch in this example
 
-// Importing the NIVODA_STAGING_USERNAME and NIVODA_STAGING_PASSWORD from the secrets.js file
-// import { NIVODA_STAGING_USERNAME, NIVODA_STAGING_PASSWORD } from '../../secrets.js';
+dotenv.config();
 const NIVODA_STAGING_USERNAME = process.env.NIVODA_STAGING_USERNAME;
 const NIVODA_STAGING_PASSWORD = process.env.NIVODA_STAGING_PASSWORD;
 import fs from 'fs'
 import { LocalStorage } from 'node-localstorage';
-
+import dotenv from 'dotenv';
 
 // You can call the GraphQL with all common request libraries such as:
 let libraries = {
